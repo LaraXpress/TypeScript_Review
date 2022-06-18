6588900c5b68285373b76cccde67d95b8f46b1ae
Mazedur (ISRT, DU) <89848156+LaraXpress@users.noreply.github.com>
2022-06-18 23:56:04 +0600

    Array types

---
+++ Data_Types/Array Types/index.ts
@@ -0,0 +1,10 @@
+let userNames:(string| number) [];
+userNames = ['mazed','mizan','mamun',100];
+console.log(userNames);
+userNames=[100,254,36,254,789];
+console.log(userNames);
+userNames.sort();
+console.log(userNames);
+
+
+
