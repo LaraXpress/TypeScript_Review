5f60c369a9ddb2a1c441bf79c92bca73bf06212e
Mazedur (ISRT, DU) <89848156+LaraXpress@users.noreply.github.com>
2022-06-18 23:54:54 +0600

    built-in-types

---
+++ Data_Types/Built-In/index.ts
@@ -0,0 +1,21 @@
+let firstName: string;
+let lastName: string;
+let fullName: string;
+let userId:number;
+
+firstName = "Sumon";
+lastName = "Das";
+fullName = firstName.concat(lastName);
+userId = 125469225;
+
+function display():void{
+    console.log('I am from Bangladesh');     
+}
+display();
+console.log(fullName);
+console.log(fullName.split(""));
+console.log(fullName.toLowerCase());
+console.log(userId.toString());
+
+
+
