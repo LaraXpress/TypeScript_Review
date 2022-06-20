ba0bd92a07cb8ac6f6d4a2a6996c8704777e50a6
Mazedur (ISRT, DU) <89848156+LaraXpress@users.noreply.github.com>
2022-06-20 19:43:07 +0600

    blogs  in Enums

---
+++ Data_Types/Enum Types/blog.ts
@@ -0,0 +1,17 @@
+enum ApprovalStatus {
+    draft,
+    submitted,
+    approved,
+    rejected
+};
+
+const request =  {
+    id: 1,
+    status: ApprovalStatus.approved,
+    description: 'Please approve this request'
+};
+
+if(request.status === ApprovalStatus.approved) {
+    // send an email
+    console.log('Send email to the Applicant...');
+}
